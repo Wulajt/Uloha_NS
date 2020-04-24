8077becd2c05ee411f586e5d061a3246f2fb91e1
janskvara <jano.skvara@outlook.sk>
2020-04-24 18:41:03 +0200

    dokumentacia a pridanjie vypoctu errorov

--- uloha2/uloha2.m
+++ uloha2/uloha2.m
@@ -1,8 +1,7 @@
 clear
 
-echo on
-load('datafun1')
-plot(x,y)
+load('datafun1');
+plot(x,y);
 
 n=length(y);
 n2=fix(n/2);
@@ -13,17 +12,27 @@ net.divideFcn='divideind';
 net.divideParam.trainInd = indx_train;
 net.divideParam.testInd = indx_test;
 
-net.trainParam.goal = 1e-4;
-net.trainParam.epochs = 1000; 
-net.trainParam.show = 5;
+net.trainParam.goal = 1e-4;   % Ukoncovacia podmienka na chybu MSE.
+net.trainParam.epochs = 100;  % Max. pocet trénovacích cyklov.
+net.trainParam.show = 5;      % Ukoncovacia podmienka na min. gradient
 
 net = train(net,x,y);
 
 outnetsim = sim(net,x);
 
+%Vypocet errorov
+SSE_train = sse(outnetsim(indx_train)-y(indx_train))
+MSE_train = immse(outnetsim(indx_train),y(indx_train))
+MAE_train = mae(outnetsim(indx_train)-y(indx_train))
+SSE_test = sse(outnetsim(indx_test)-y(indx_test))
+MSE_test = immse(outnetsim(indx_test),y(indx_test))
+MAE_test = mae(outnetsim(indx_test)-y(indx_test))
 % Vykreslenie priebehov
 figure
 plot(x,y,'b',x,outnetsim,'-or')
+ylabel('y')
+xlabel('x')
+legend('Dana funkcia','Data z NS')
 echo off
 
 
