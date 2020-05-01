799422a02c25919cb80da430b3d411c379cda37a
janskvara <jano.skvara@outlook.sk>
2020-05-01 13:11:01 +0200

    udaje na  ktorych bola robena dokumentacia

--- uloha3/testovanie_10.m
+++ uloha3/testovanie_10.m
@@ -2,7 +2,8 @@ clear
 
 load('datapiscisla_all.mat');
 
-S1 = 20;
+
+S1 = 24;
 
 best_succ_rate = 0;
 min_succ_rate = 0;
@@ -19,11 +20,10 @@ for i = 1:10
     net.divideParam.valRatio=0;
 
     % nastavenie parametrov trenovania epoch
-    net.trainParam.goal = 0.000001;     % error goal.
-    net.trainParam.show = 20;           % Frequency of progress displays (in epochs).
-    net.trainParam.epochs = 1000;       % Maximum number of epochs to train.
-    net.trainParam.min_grad = 1e-10;    % ukoncovacia podmienka na min. gradient 
-
+    net.trainParam.goal = 0.000001;    % error goal.
+    net.trainParam.show = 20;          % Frequency of progress displays(in epochs).
+    net.trainParam.epochs = 1000;      % Maximum number of epochs to train.
+    net.trainParam.min_grad = 1e-5;    % ukoncovacia podmienka na min. gradient
     % trenovanie NS
     net = train(net,XDataall,YDataall);
     
--- uloha3/uloha3.m
+++ uloha3/uloha3.m
@@ -2,7 +2,7 @@ clear
 
 load('datapiscisla_all.mat');
 
-S1 = 20;
+S1 = 24;
 net = patternnet(S1);
 
 % vsetky data pouzite na trenovanie
@@ -12,20 +12,24 @@ net.divideParam.testRatio=0.4;
 net.divideParam.valRatio=0;
 
 % nastavenie parametrov trenovania epoch
-net.trainParam.goal = 0.000001;     % error goal.
-net.trainParam.show = 20;           % Frequency of progress displays (in epochs).
-net.trainParam.epochs = 1000;        % Maximum number of epochs to train.
-net.trainParam.min_grad = 1e-10;    % ukoncovacia podmienka na min. gradient 
+net.trainParam.goal = 0.000001;    % error goal.
+net.trainParam.show = 20;          % Frequency of progress displays(in epochs).
+net.trainParam.epochs = 1000;      % Maximum number of epochs to train.
+net.trainParam.min_grad = 1e-5;    % ukoncovacia podmienka na min. gradient 
 
 % trenovanie NS
 net = train(net,XDataall,YDataall);
 
 outnetsim = sim(net,XDataall);
+YPredicted = net(XDataall);
 classes_predict = vec2ind(outnetsim);
 classes_true = vec2ind(YDataall);
 confmat = confusionmat(classes_true,classes_predict)/4940*100
 succesfull_rate = trace(confmat)
+plotconfusion(YDataall,YPredicted)
 
+% vytahovanie objektov z datasetu a priradenie objektu predpokladane
+%cislo na zaklade natrenovanej neurpnovej siete
 for i = 100:500:4600
     dataX= XDataall(:,i);
     dispznak(dataX,28,28);
